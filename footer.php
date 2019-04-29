60137778501fb4b635bead2911337d1ed1d97e0e
Mozin Omer <mozinomer12@gmail.com>
2019-04-29 12:30:28 +0500

    final single blog post page

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
 
-<?php get_footer();?>
+<?php wp_footer();?>
 </body>
 
 </html>
--- single.php
+++ single.php
@@ -1,15 +1,29 @@
 <?php get_header();
 while (have_posts() ) : the_post();
 ?>
-
+<div class="header_single_blog">
+	<div class="container">
+		<ul class="disflex main_navigation_single_blog_page">
+			<li>
+				home &nbsp &nbsp  >
+			</li>
+			<li>
+				our blog  &nbsp &nbsp>
+			</li>
+			<li>
+				<?php the_title();?>
+			</li>
+		</ul>
+	</div>
+</div>
 
 <div class="single_post_blog">
 	<div class="container">
-		<a href="blog" class="underlineBtn underlineBtn--black">
+		<a href="http://localhost/equfin/blog/" class="underlineBtn underlineBtn--black blog_link_single_post">
 			<i class="fas fa-arrow-left"></i> 
 			Back to Our Blog
 		</a>
-		<div class="col-12 col-lg-8 offset-lg-2">
+		<div class="col-12 col-lg-8 offset-lg-2 main_content_blog_single">
 			<div class="row the_post_thumbnail_single_blog">
 				<?php the_post_thumbnail();?>
 			</div>
