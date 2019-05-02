542ae57e5b52c341b2607c675dcd69bba603ceae
Mozin Omer <mozinomer12@gmail.com>
2019-05-02 18:22:02 +0500

    responsive design

--- page-home.php
+++ page-home.php
@@ -95,7 +95,7 @@ if ( have_posts() ) {
 			Heritage BPO in Numbers
 		</div>
 		<div class="row mt-2 main_row_section_3_section-1">
-            <div class="col-sm-6 col-lg-3 section1Anim">
+            <div class="col-sm-12 col-lg-3 section1Anim">
                 <div class="row">
                 	<div class="col-sm-12">
                 		<h2 class="h2_purple">BPO Processes completed
@@ -107,7 +107,7 @@ if ( have_posts() ) {
                 		<span class="timer count-number" data-to="1" data-speed="700">8</span>+</div>
                 </div>
             </div>
-            <div class="col-sm-6 col-lg-3 section1Anim mt-5 mt-sm-0">
+            <div class="col-sm-12 col-lg-3 section1Anim mt-5 mt-sm-0">
                 <div class="row">
                 	<div class="col-sm-12">
                 		<h2 class="h2_purple">BPM & BPO Consultants
@@ -120,7 +120,7 @@ if ( have_posts() ) {
                 		<span class="timer count-number" data-to="1" data-speed="700">12</span>+</div>
                 </div>
             </div>
-            <div class="col-sm-6 col-lg-3 mt-5 mt-lg-0 section1Anim">
+            <div class="col-sm-12 col-lg-3 mt-5 mt-lg-0 section1Anim">
                 <div class="row">
                 	<div class="col-sm-12">
                 		<h2 class="h2_purple">Awards & Recognitions
@@ -133,7 +133,7 @@ if ( have_posts() ) {
                 		<span class="timer count-number" data-to="2.3" data-speed="700">3</span>+</div>
                 </div>
             </div>
-            <div class="col-sm-6 col-lg-3 mt-5 mt-lg-0 section1Anim">
+            <div class="col-sm-12 col-lg-3 mt-5 mt-lg-0 section1Anim">
                 <div class="row">
                 	<div class="col-sm-12">
                 		<h2 class="h2_purple">Satisfied Clients
@@ -157,7 +157,7 @@ if ( have_posts() ) {
                     </div>
                     <div class="row mt-8 mt-sm-12">
                     	<div class="col-sm-12 animatedContent">
-                    		<a href="investorsoverview" class="buttonWhite animatedButton" style="--x:115px;--y:">
+                    		<a href="investorsoverview" class="buttonWhite animatedButton button_asd" style="--x:115px;--y:">
                     		<span>JOIN OUR JOURNEY</span></a>
                     	</div>
                     </div>
@@ -180,7 +180,7 @@ if ( have_posts() ) {
                     	</div>
                     </div>
                     <div class="row">
-                    	<div class="col-sm-12 section1CAnim col-lg-8 offset-lg-2">
+                    	<div class="col-sm-12 section1CAnim col-lg-8 offset-lg-2 para_ns_media">
                             Heritage BPO offers a lineup of Financial products, backed up by robust in-house platforms,
                             that deliver real solutions to consumer needs.
                         </div>
@@ -188,50 +188,50 @@ if ( have_posts() ) {
                     <div class="row mt-6 mt-sm-16">
                     	<div class="col-sm-12 col-lg-10 offset-lg-1">
                             <div class="row">
-                                <div class="col-sm-6 col-lg-3 home__section1__icon section1DAnim">
+                                <div class="col-sm-12 col-lg-3 home__section1__icon section1DAnim">
                                     <div class="row">
                                     	<div class="col-sm-12"><img src="<?php echo get_template_directory_uri(); ?>/images/homeIcon1.png" class="fluid-img" alt="Mortgage Loans"></div>
                                     </div>
                                     <div class="row mt-3">
-                                    	<div class="col-sm-12">Industry BPO Solution</div>
+                                    	<div class="col-sm-12 payload_media">Industry BPO Solution</div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6 col-lg-3 mt-12 mt-sm-0 mt-md-0 home__section1__icon section1DAnim">
+                                <div class="col-sm-12 col-lg-3 mt-12 mt-sm-0 mt-md-0 home__section1__icon section1DAnim">
                                     <div class="row">
                                     	<div class="col-sm-12"><img src="<?php echo get_template_directory_uri(); ?>/images/homeIcon2.png" class="fluid-img" alt="Mortgage Loans"></div>
                                     </div>
                                     <div class="row mt-3">
-                                    	<div class="col-sm-12">eCommerce Solution</div>
+                                    	<div class="col-sm-12 payload_media">eCommerce Solution</div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6 col-lg-3 mt-12 mt-sm-5 mt-lg-0 home__section1__icon section1DAnim">
+                                <div class="col-sm-12 col-lg-3 mt-12 mt-sm-5 mt-lg-0 home__section1__icon section1DAnim">
                                     <div class="row">
                                     	<div class="col-sm-12"><img src="<?php echo get_template_directory_uri(); ?>/images/homeIcon3.png" class="fluid-img" alt="Mortgage Loans"></div>
                                     </div>
                                     <div class="row mt-3">
-                                    	<div class="col-sm-12">Financial Services</div>
+                                    	<div class="col-sm-12 payload_media">Financial Services</div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6 col-lg-3 mt-12 mt-sm-5 mt-lg-0 home__section1__icon section1DAnim">
+                                <div class="col-sm-12 col-lg-3 mt-12 mt-sm-5 mt-lg-0 home__section1__icon section1DAnim">
                                     <div class="row">
                                     	<div class="col-sm-12"><img src="<?php echo get_template_directory_uri(); ?>/images/homeIcon4.png" class="fluid-img" alt="Mortgage Loans"></div>
                                     </div>
                                     <div class="row mt-3">
-                                    	<div class="col-md-8 offset-md-2 col-lg-12 offset-lg-0">Online / Offline Payday Loans</div>
+                                    	<div class="col-md-8 offset-md-2 col-lg-12 offset-lg-0 payload_media">Online / Offline Payday Loans</div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="row mt-12 section1EAnim">
-                    	<div class="col-sm-12 col-lg-8 offset-lg-2">
+                    	<div class="col-sm-12 col-lg-8 offset-lg-2 para_ns_media">
                             Fully compliant, our avant-garde technology, simply delivers. Minimizing the time, steps
                             and difficulty in obtaining access and management to funds.
                         </div>
                     </div>
                     <div class="row mt-8 section1EAnim">
                     	<div class="col-sm-12">
-                    		<a href="whatwedo" class="buttonBlue animatedButton">
+                    		<a href="whatwedo" class="buttonBlue animatedButton btn_load_nmore">
                     		<span>LEARN MORE</span></a>
                     	</div>
                     </div>
@@ -244,22 +244,22 @@ if ( have_posts() ) {
 	<div class="main_container_home_page_bg_blog">
 		<div class="container blog_container_homepage">
 			<div class="row">
-				<div class="col-sm-12 section3AAnim">
-					<h2>BLOG HEADLINES
+				<div class="col-sm-12 section3AAnim ">
+					<h2 class="heading_homder_page">BLOG HEADLINES
 					</h2>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-sm-12 section3AAnim">
-					<h3>
+					<h3 class="heaidng_main_homeropage">
 						Latest Blog posts &amp; News
 					</h3>
 				</div>
 			</div>
-			<div class="row mt-4">
+			<div class="row mt-4 row0blog">
 			<?php $data_query =  new WP_Query(array('post_type' => 'post', 'order_by'=>'Des', 'posts_per_page' => 3));?>
 			<?php while ( $data_query->have_posts() ) : $data_query->the_post();?>
-				<div class="col-sm-6 col-md-4 blogAnim">
+				<div class="col-sm-12 col-md-4 blogAnim">
 					<a href="<?php the_permalink();?>">
 					<div class="row the_post_thumbnail_blog">
 						<?php the_post_thumbnail();?>
@@ -284,8 +284,8 @@ if ( have_posts() ) {
 		</div>
 		<div class="row mt-7">
 			<div class="col-sm-12">
-				<a href="http://localhost/Heritage BPO/blog/" class="buttonBlue animatedButton" style="--x:125px;--y:">
-					<span>
+				<a href="http://localhost/Heritage BPO/blog/" class="buttonBlue animatedButton btn_load_nmore" style="--x:125px;--y:">
+					<span class="">
 						VISIT OUR BLOG
 					</span>
 				</a>
